ad9d14348c7a27318b81c23b1adf2b6f291ca51f
Rutger Farry <rutgerfarry@gmail.com>
2018-01-15 15:02:12 -0800

    Initial Commit

---
+++ DataKit/DataKit.h
@@ -0,0 +1,19 @@
+//
+//  DataKit.h
+//  DataKit
+//
+//  Created by Rutger Farry on 1/15/18.
+//  Copyright © 2018 Rutger Farry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataKit.
+FOUNDATION_EXPORT double DataKitVersionNumber;
+
+//! Project version string for DataKit.
+FOUNDATION_EXPORT const unsigned char DataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataKit/PublicHeader.h>
+
+
